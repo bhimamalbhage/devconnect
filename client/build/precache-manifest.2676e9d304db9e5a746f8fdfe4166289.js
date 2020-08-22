e3555d352dfa9d54092856ac005331e319759016
Bhimashankar <bhimamalbhage@hotmail.com>
2020-08-22 20:02:08 +0530

    first commit

---
+++ client/build/precache-manifest.2676e9d304db9e5a746f8fdfe4166289.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8d24041334acdc3aa8969818d0b3bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca4cbfd884c0581c5da3",
+    "url": "/static/css/main.7d9d1bb1.chunk.css"
+  },
+  {
+    "revision": "4bac8d731e97a6045fc6",
+    "url": "/static/js/2.cc80da2f.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.cc80da2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4cbfd884c0581c5da3",
+    "url": "/static/js/main.9125e1cb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
